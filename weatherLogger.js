f23427ff55a2d3dd5d132eccd0cbedb48b8c92a6
okputadora <okputadora@gmail.com>
2018-04-06 14:31:27 -0400

    init

---
+++ weatherLogger.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports.wunderground = (event, context, callback) => {
+  const response = {
+    
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
